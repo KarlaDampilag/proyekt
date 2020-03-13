abbea11ff0bc714586e6114c08f805e5836ed7cc
Karla Dampilag <karla.dmplg@gmail.com>
2020-03-14 03:19:43 +0800

    change frontend config.js and withData.js

--- frontend/config.js
+++ frontend/config.js
@@ -1,4 +1,4 @@
 // This is client side config only - don't put anything in here that shouldn't be public!
 export const endpoint = `http://localhost:4444`;
 export const perPage = 4;
-export const prodEndpoint = 'https://sick-fits-test-86d8c2a687.herokuapp.com/';
+export const prodEndpoint = 'https://sickfits-yoga-test2.herokuapp.com/';
