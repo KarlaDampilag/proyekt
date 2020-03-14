2a34124822d77fc1e221519d3911f4e5198f3014
Karla Dampilag <karla.dmplg@gmail.com>
2020-03-14 18:42:44 +0800

    add pwa

--- frontend/components/Meta.js
+++ frontend/components/Meta.js
@@ -8,6 +8,12 @@ const Meta = () => (
         <link rel="stylesheet" type="text/css" href="/static/general.css" />
         <link rel="stylesheet" type="text/css" href="/static/nprogress.css" />
         <title>Sick Fits!</title>
+
+        <link rel="manifest" href="/manifest.json" />
+        <link href='/favicon-16x16.png' rel='icon' type='image/png' sizes='16x16' />
+        <link href='/favicon-32x32.png' rel='icon' type='image/png' sizes='32x32' />
+        <link rel="apple-touch-icon" href="/apple-icon.png"></link>
+        <meta name="theme-color" content="#317EFB" />
     </Head>
 )
 
