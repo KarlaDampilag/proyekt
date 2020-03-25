2a1edbc0c60526e16ffb54ffaa44bcb74d849fc7
Karla Dampilag <karla.dmplg@gmail.com>
2020-03-26 03:03:19 +0800

    adds AddProductButton component, refactors Products page

---
+++ frontend/components/AddProductButton.js
@@ -0,0 +1,240 @@
+import { Mutation, Query } from 'react-apollo';
+import gql from 'graphql-tag';
+import { Modal, Button, Input, Form, Select, Spin, message } from 'antd';
+
+import { ALL_PRODUCTS_QUERY } from './Products';
+import CategoriesInput from './CategoriesInput';
+
+const CREATE_PRODUCT_MUTATION = gql`
+mutation CREATE_PRODUCT_MUTATION(
+    $name: String!
+    $salePrice: String!
+    $costPrice: String
+    $unit: String
+    $notes: String
+    $image: String
+    $largeImage: String
+    $categories: [String!]
+) {
+    createProduct(
+        name: $name
+        salePrice: $salePrice
+        costPrice: $costPrice
+        unit: $unit
+        notes: $notes
+        image: $image
+        largeImage: $largeImage
+        categories: $categories
+    ) {
+        id
+        name
+        salePrice
+        costPrice
+        unit
+        notes
+        image
+        largeImage
+        categories
+        createdAt
+    }
+}
+`;
+
+const ALL_CATEGORIES_QUERY = gql`
+    query ALL_CATEGORIES_QUERY {
+        categories {
+            id
+            name
+        }
+    }
+`;
+
+const CREATE_CATEGORIES_MUTATION = gql`
+    mutation CREATE_CATEGORIES_MUTATION($names: [String!]!) {
+        createCategories(names: $names)
+    }
+`;
+
+const AddProductButton = () => {
+    const [name, setName] = React.useState();
+    const [salePrice, setSalePrice] = React.useState();
+    const [costPrice, setCostPrice] = React.useState();
+    const [unit, setUnit] = React.useState();
+    const [notes, setNotes] = React.useState();
+    const [image, setImage] = React.useState();
+    const [largeImage, setLargeImage] = React.useState();
+    const [categories, setCategories] = React.useState();
+    const [newCategories, setNewCategories] = React.useState();
+    const [isShowingModal, setIsShowingModal] = React.useState(false);
+    const [isLoading, setIsLoading] = React.useState();
+
+    const [form] = Form.useForm();
+
+    const uploadFile = async (e) => {
+        const files = e.target.files;
+        const data = new FormData();
+        data.append('file', files[0]);
+        data.append('upload_preset', 'sickfits'); // needed by Cloudinary
+
+        setIsLoading(true);
+        const response = await fetch('https://api.cloudinary.com/v1_1/dlki0o7xf/image/upload', {
+            method: 'POST',
+            body: data
+        });
+
+        const file = await response.json();
+        if (file && file.secure_url && file.eager) {
+            setImage(file.secure_url);
+            setLargeImage(file.eager[0].secure_url);
+        } else {
+            setImage(null);
+            setLargeImage(null);
+        }
+        setIsLoading(false);
+    }
+
+    const update = (cache, payload) => {
+        // Read cache for the products
+        const data = cache.readQuery({ query: ALL_PRODUCTS_QUERY });
+
+        // Add the new product
+        data.products.push(payload.data.createProduct);
+        data.products = _.sortBy(data.products, 'createdAt');
+
+        // Put the updated products back in the cache
+        cache.writeQuery({ query: ALL_PRODUCTS_QUERY, data })
+    }
+
+    const renderTriggerButton = () => {
+        return (
+            <Button onClick={() => setIsShowingModal(true)}>AddProduct</Button>
+        );
+    }
+
+    return (
+        <>
+            <Mutation
+                mutation={CREATE_PRODUCT_MUTATION}
+                variables={{ name, salePrice, costPrice, unit, notes, image, largeImage, categories }}
+                update={update}
+            >
+                {(createProduct, { loading, error }) => (
+                    <Mutation mutation={CREATE_CATEGORIES_MUTATION} variables={{ names: newCategories }}>
+                        {(createCategories, { loading, error }) => (
+                            <Modal title='Add a Product' visible={isShowingModal} onCancel={() => setIsShowingModal(false)} footer={null}>
+                                <Form {...layout} form={form} onFinish={async () => {
+                                    let response = await createProduct();
+
+                                    if (newCategories && newCategories.length > 0) {
+                                        response = await createCategories();
+                                    }
+
+                                    if (error) {
+                                        message.error(error.message.replace('GraphQL error: ', ''));
+                                    } else {
+                                        setIsShowingModal(false);
+                                        form.resetFields();
+                                        message.success('Product added');
+                                    }
+
+                                }}>
+                                    <Form.Item
+                                        label="Name"
+                                        name="name"
+                                        rules={[{ required: true, message: 'This field is required' }]}
+                                    >
+                                        <Input value={name} onChange={e => setName(e.target.value)} />
+                                    </Form.Item>
+
+                                    <Form.Item
+                                        label="Sale Price"
+                                        name="salePrice"
+                                        rules={[{ required: true, message: 'This field is required' }]}
+                                    >
+                                        <Input type='number' onChange={e => setSalePrice(e.target.value.toString())} />
+                                    </Form.Item>
+
+                                    <Form.Item
+                                        label="Cost Price"
+                                        name="costPrice"
+                                    >
+                                        <Input type='number' onChange={e => setCostPrice(e.target.value.toString())} />
+                                    </Form.Item>
+
+                                    <Form.Item
+                                        label="Unit"
+                                        name="unit"
+                                    >
+                                        <Input value={unit} onChange={e => setUnit(e.target.value)} />
+                                    </Form.Item>
+
+                                    <Form.Item
+                                        label="Categories"
+                                        name="categories"
+                                    >
+                                        <Query query={ALL_CATEGORIES_QUERY}>
+                                            {({ data }) => {
+                                                const options = _.map(data.categories, category => category.name);
+                                                return (
+                                                    <Select
+                                                        value={categories}
+                                                        mode='tags'
+                                                        placeholder='Start typing to add...'
+                                                        onChange={value => {
+                                                            setCategories(value);
+                                                            const newCategoriesToSave = _.filter(value, category => options.indexOf(category) < 0);
+                                                            setNewCategories(newCategoriesToSave);
+                                                        }}
+                                                    >
+                                                        {
+                                                            _.map(options, (option, key) => (
+                                                                <Select.Option value={option} key={key}>{option}</Select.Option>
+                                                            ))
+                                                        }
+                                                    </Select>
+                                                );
+                                            }}
+                                        </Query>
+                                    </Form.Item>
+
+                                    <Form.Item
+                                        label="Notes"
+                                        name="notes"
+                                    >
+                                        <Input value={notes} onChange={e => setNotes(e.target.value)} />
+                                    </Form.Item>
+
+                                    <Form.Item
+                                        label="Image"
+                                        name="image"
+                                    >
+                                        <Input type='file' placeholder='Upload an image' onChange={uploadFile} />
+                                        {isLoading && <Spin />}
+                                        {image && <img src={image} width='200' alt='upload preview' />}
+                                    </Form.Item>
+
+                                    <Form.Item {...tailLayout}>
+                                        <Button type="primary" htmlType="submit" disabled={isLoading || loading}>Add{loading && 'ing'} Product</Button>
+                                        <Button onClick={() => setIsShowingModal(false)}>Cancel</Button>
+                                    </Form.Item>
+                                </Form>
+                            </Modal>
+                        )}
+                    </Mutation>
+                )}
+            </Mutation>
+            {renderTriggerButton()}
+        </>
+    )
+}
+
+const layout = {
+    labelCol: { span: 8 },
+    wrapperCol: { span: 16 },
+};
+const tailLayout = {
+    wrapperCol: { offset: 8, span: 16 },
+};
+
+export default AddProductButton;
+export { layout, tailLayout };
--- frontend/components/Products.js
+++ frontend/components/Products.js
@@ -2,20 +2,23 @@ import * as _ from 'lodash';
 import { Query } from 'react-apollo';
 import gql from 'graphql-tag';
 import Link from 'next/link'
-import { Table, Modal, Button, Input, Form, Spin, Select, message } from 'antd';
+import { Tag, Table, Button } from 'antd';
 
 import { userContext } from './Page';
 import { perPage } from '../config';
 import ErrorMessage from './ErrorMessage';
+import AddProductButton from './AddProductButton';
 
 const ALL_PRODUCTS_QUERY = gql`
-    query ALL_PRODUCTS_QUERY($skip: Int = 0, $first: Int = ${perPage}) {
-        products(first: $first, skip: $skip, orderBy: createdAt_DESC) {
+    query ALL_PRODUCTS_QUERY {
+        products(orderBy: createdAt_DESC) {
             id
             name
             salePrice
+            costPrice
             unit
             categories
+            notes
             image
         }
     }
@@ -29,76 +32,88 @@ const Products = (props) => {
                     return <p>You must be logged in to access this page.</p>
                 }
                 return (
-                    <Query query={ALL_PRODUCTS_QUERY} variables={{ skip: props.page * perPage - perPage }}>
-                        {({ error, loading, data }) => {
-                            if (error) return <ErrorMessage error={error} />
-                            return (
-                                <div>
-                                    <Table
-                                        loading={loading}
-                                        dataSource={data.products}
-                                        rowKey='id'
-                                        expandable={{
-                                            expandedRowRender: record => {
-                                                if (record.description) {
-                                                    return <p style={{ margin: 0 }}>{record.description}</p>
-                                                }
-                                                return <p style={{ margin: 0 }}>This product has no description.</p>
-                                            }
-                                        }}
-                                        columns={[
-                                            {
-                                                dataIndex: 'image',
-                                                render: (value, record) => {
-                                                    if (value) {
-                                                        return <img src={value} alt={record.name} width='150px' />
+                    <>
+                        <div><AddProductButton /></div>
+                        <Query query={ALL_PRODUCTS_QUERY} variables={{ skip: props.page * perPage - perPage }}>
+                            {({ error, loading, data }) => {
+                                if (error) return <ErrorMessage error={error} />
+                                return (
+                                    <div>
+                                        <Table
+                                            loading={loading}
+                                            dataSource={data.products}
+                                            rowKey='id'
+                                            expandable={{
+                                                expandedRowRender: record => {
+                                                    if (record.notes) {
+                                                        return <p style={{ margin: 0 }}>{record.notes}</p>
                                                     }
-                                                    return null;
+                                                    return <p style={{ margin: 0 }}>This product has no notes.</p>
                                                 }
-                                            },
-                                            {
-                                                title: 'Name',
-                                                dataIndex: 'name'
-                                            },
-                                            {
-                                                title: 'Cost Price',
-                                                dataIndex: 'costPrice'
-                                            },
-                                            {
-                                                title: 'Sale Price',
-                                                dataIndex: 'salePrice'
-                                            },
-                                            {
-                                                title: 'Edit ✏️',
-                                                dataIndex: 'id',
-                                                key: 'edit',
-                                                render: (value) => {
-                                                    return (
-                                                        <Link href={{
-                                                            pathname: '/update',
-                                                            query: { id: value }
-                                                        }}>
-                                                            <a>Edit ✏️</a>
-                                                        </Link>
-                                                    );
+                                            }}
+                                            columns={[
+                                                {
+                                                    dataIndex: 'image',
+                                                    render: (value, record) => {
+                                                        if (value) {
+                                                            return <img src={value} alt={record.name} width='150px' />
+                                                        }
+                                                        return null;
+                                                    }
+                                                },
+                                                {
+                                                    title: 'Name',
+                                                    dataIndex: 'name'
+                                                },
+                                                {
+                                                    title: 'Sale Price',
+                                                    dataIndex: 'salePrice'
+                                                },
+                                                {
+                                                    title: 'Cost Price',
+                                                    dataIndex: 'costPrice'
+                                                },
+                                                {
+                                                    title: 'Categories',
+                                                    dataIndex: 'categories',
+                                                    render: (value) => {
+                                                        return _.map(value, category => {
+                                                            return <Tag key={category}>{category}</Tag>
+                                                        })
+                                                    }
+                                                },
+                                                {
+                                                    title: 'Edit ✏️',
+                                                    dataIndex: 'id',
+                                                    key: 'edit',
+                                                    render: (value) => {
+                                                        return (
+                                                            <Link href={{
+                                                                pathname: '/update',
+                                                                query: { id: value }
+                                                            }}>
+                                                                <a><Button>Edit</Button></a>
+                                                            </Link>
+                                                        );
+                                                    }
+                                                },
+                                                {
+                                                    title: 'Delete ',
+                                                    dataIndex: 'id',
+                                                    key: 'edit',
+                                                    render: (value) => {
+                                                        return (
+                                                            <Button>Delete</Button>
+                                                        );
+                                                    }
                                                 }
-                                            },
-                                            {
-                                                title: 'Delete ',
-                                                dataIndex: 'id',
-                                                key: 'edit',
-                                                render: (value) => {
-                                                    return (
-                                                        <Button>Delete</Button>
-                                                    );
-                                                }
-                                            }
-                                        ]}
-                                    />
-                                </div>
-                            );
-                        }}
-                    </Query>
+                                            ]}
+                                        />
+                                    </div>
+                                );
+                            }}
+                        </Query>
+                    </>
                 );
             }}
         </userContext.Consumer>
